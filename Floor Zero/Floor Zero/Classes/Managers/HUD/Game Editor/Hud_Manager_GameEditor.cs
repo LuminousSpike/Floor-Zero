83b8efb71dc7ff9119da9f4d991422a6e0790143
Nathan <nathan.j.todd@gmail.com>
2013-05-20 10:33:59 +1200

    Created Hud_Manager_GameEditor class with skeleton code.

---
+++ Floor Zero/Floor Zero/Classes/Managers/HUD/Game Editor/Hud_Manager_GameEditor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Solar.GUI;
+using Solar.GUI.Controls;
+
+namespace Floor_Zero.Classes.Managers.HUD.Game_Editor
+{
+    class Hud_Manager_GameEditor
+    {
+        GuiSystem hudSystem = new GuiSystem();
+
+        public void Initialize()
+        {
+            hudSystem.Initialize();
+        }
+
+        public void LoadContent(ContentManager Content, GraphicsDevice graphicsDevice)
+        {
+            hudSystem.LoadContent(Content, graphicsDevice);
+        }
+
+        public void UnloadContent()
+        {
+            hudSystem.UnloadContent();
+        }
+
+        public void Update()
+        {
+            hudSystem.Update();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            hudSystem.Draw(spriteBatch);
+        }
+    }
+}
